06a9001c004a8d5c78bb78ea2d5a709a4db37671
Gabotox <108927775+Gabotox@users.noreply.github.com>
2022-12-14 18:36:33 -0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,62 @@
+var elnumero = Math.floor(Math.random()*10+1);
+var minumero = document.getElementById("input1").value;
+
+
+
+function cambiar(){
+
+    
+minumero = document.getElementById("input1").value;
+    
+
+    if (minumero > elnumero) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Muy alto, intenta de nuevo',
+            showConfirmButton: false,
+            timer: 1000
+          })
+          
+    } else {	
+        Swal.fire({
+            icon: 'error',
+            title: 'Muy bajo, intenta de nuevo',
+            showConfirmButton: false,
+            timer: 1000
+          })
+          
+    }
+    
+   
+    
+    document.getElementById("p").innerHTML="Te quedan menos intentos";
+    var minumero = document.getElementById("input1").value;
+    
+
+
+    if (minumero == elnumero){
+        Swal.fire({
+            title: 'Adivinaste el número secreto',
+            width: 400,
+            padding: '0.5rem',
+            color: '#716add',
+            background: '#fff url(/images/trees.png)',
+            backdrop: `
+              rgba(0,0,123,0.4)
+              url("../img/cat.gif")
+              left top
+              no-repeat
+            `
+          })
+    }
+        
+        if (minumero == "") {
+            Swal.fire({
+                icon: 'error',
+                title: 'Ingresa un número primero',
+                showConfirmButton: false,
+                timer: 1500
+              })
+            }
+
+}
